b840588fa8baeed4325baa492f4bf34189254689
Dmytro Nevoit <119137577+Nevojt@users.noreply.github.com>
2023-04-07 15:35:18 +0200

    Update main.py
    New feature, plus error handing

--- NATO-alphabet-start/main.py
+++ NATO-alphabet-start/main.py
@@ -4,11 +4,14 @@ import pandas as pd
 data = pd.read_csv("nato_phonetic_alphabet.csv")
 dictionary = {row.letter:row.code for (index, row) in data.iterrows()}
 
-
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-user_input = input("What is word NATO?: ").upper()
-nato_list = []
-            
-[nato_list.append(value) for letter in user_input for (key, value) in dictionary.items() if letter == key]       
-            
-print(nato_list)
+def generate_phonetic_code():
+    user_input = input("What is word NATO?: ").upper()
+    try:
+        nato_list = [dictionary[letter] for letter in user_input]       
+    except KeyError:
+        print("Sorry, only letters the alphabet please.")
+        generate_phonetic_code()
+    else:
+        print(nato_list)
+        
+generate_phonetic_code()
