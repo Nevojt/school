0e2c9a8755073e0730f765527eb140efc85f7500
Nevojt <nevojt@gmail.com>
2023-12-23 21:08:20 +0100

    new games

--- dangeon_hanter.py
+++ dangeon_hanter.py
@@ -4,7 +4,7 @@ print("Перед тобою дві двері яку ти вибереш?")
 walk = input("Left or Right?\n").lower()
 
 if walk == "left":
-    print("Ти зустрів Огра").upper()
+    print("Ти зустрів Огра")
     
     battle = input("Тікати чи битись?\n Yes or No?").lower()
     if battle == "yes":
@@ -15,27 +15,62 @@ if walk == "left":
         if do == "yes":
             print("Він каже зробити вінок.")
             
-            winok = input("Yes or No").lower()
+            winok = input("Yes or No\n").lower()
             if winok == "yes":
-                print("")
+                print("Огр на тобі одружується, проти твоєї волі!!!")
                 
             elif winok == "no":
                 print("Огр б'є нас квітами по сраці!!!")
+                
+            else:
+                print("Bad input")
             
             
         elif do == "no":
             print("Огр плаче.")
+            
+        else:
+            print("Bad input")
         
         
         
     elif battle == "no":
-        print("Ти налякав Огра і він втікає.").upper()
-    
-    
-    
-    
+        print("Ти налякав Огра і він втікає.")
+        
+    else:
+        print("Bad input")
     
     
     
 elif walk == "right":
     print("За дверима ти побачив скриньку.")
+    box = input("Відкрити чи ні. Yes or No ").lower()
+    
+    if box == "Yes":
+        print("З неї вилазить Аня.")
+        print("І затягує тебе в середину")
+        
+        print("Аня каже тобі\n-Допоможи мені будь ласка")
+        
+        
+        need_anna = input("Yes or No")
+        
+        if need_anna == "Yes":
+            print("")
+            
+        elif need_anna == "No":
+            print("")
+            
+            
+    
+    elif box == "No":
+        print()
+        
+    else:
+        print("Bad input")
+    
+        
+    
+    
+else:
+    print("Bad input")
--- game_hunter.py
+++ game_hunter.py
@@ -12,6 +12,7 @@ walk = input().lower()
 if walk == "RIGHT":
     print("Ти вмер АХАХААХА")
     
+    
 elif walk == "LEFT":
     print("Ти знайшов кристал!")
     print("Підняти чи ні?")
@@ -20,17 +21,44 @@ elif walk == "LEFT":
     if gams == "Yes":
         print("Ти отримав кристал.")
         print("Цей кристал був Огра і він тебе помітив.")
+        
         battle = input("Тікати чи битись?\n Yes or No?").lower()
         
         if battle == "Yes":
-            print("")
+            print("Огр дає тобі підсрачник і ти вилетів з печеру")
+            print("Game Over")
             
         elif battle == "No":
-            print("")
+            print("Огр тебе наздоганяє.")
+            print("Game Over")
+        else:
+            print("Bad input.")
         
         
     elif gams == "No":
         print("Йдеш далі...")
+        print("Ти побачив дві двері. В яку зайдеш?")
+        
+        door = input("В яку зайдеш? Left or Right\n").lower()
+        if door == "left":
+            print("За дверима була пастка в яку ти потрапив")
+            print("Game Over")
+            
+        elif door == "right":
+            print("За дверима ти знайшов принцесу.")
+        else:
+            print("Bad input.")
+            
+    else:
+        print("Bad input.")
+            
+else:
+    print("Bad input.")
+            
+        
+        
+        
+        
 
 
 
