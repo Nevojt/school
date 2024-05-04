72d500c2639803eefd1d89c3f5348a977eb0d7d7
Nevojt <nevojt@gmail.com>
2024-05-04 13:04:33 +0200

    update oop

--- OOP/lesson_1.py/learn_my.py
+++ OOP/lesson_1.py/learn_my.py
@@ -44,30 +44,60 @@
 # print(number_of_3_cars)
 
 
-class Circle:
-    pi = 3.14
+# class Circle:
+#     pi = 3.14
     
-    def __init__(self, radius: int = 1) -> None:
-        self.radius = radius
-        self.circumference = 2 * self.pi * self.radius
+#     def __init__(self, radius: int = 1) -> None:
+#         self.radius = radius
+#         self.circumference = 2 * self.pi * self.radius
         
-    def get_area(self):
-        return self.pi * (self.radius ** 2)
+#     def get_area(self):
+#         return self.pi * (self.radius ** 2)
     
-    def get_circumference(self):
-        return 2 * self.pi * self.radius
+#     def get_circumference(self):
+#         return 2 * self.pi * self.radius
     
 
-circle_1 = Circle(radius=5)
-circle_2 = Circle(radius=30)
-circle_3 = Circle()
+# circle_1 = Circle(radius=5)
+# circle_2 = Circle(radius=30)
+# circle_3 = Circle()
 
-print(circle_3.radius)
+# print(circle_3.radius)
 
-print(circle_1.circumference)
+# print(circle_1.circumference)
 
-print(circle_1.get_area())
-print(circle_1.get_circumference())
+# print(circle_1.get_area())
+# print(circle_1.get_circumference())
+ 
+# print(circle_2.get_area())
+# print(circle_2.get_circumference())
 
-print(circle_2.get_area())
-print(circle_2.get_circumference())
+
+
+class Item:
+    
+    pay_rate = 0.8
+    def __init__(self, name: str, price: int, quantity: int = 0) -> None:
+        assert price >= 0, f"Price {price} is not greater that zero"
+        assert quantity >= 0, f"Quantity {quantity} is not greater than zero"
+        
+        
+        self.price = price
+        self.name = name
+        self.quantity = quantity
+        
+    # def calculated_total_price(self, x, y):
+    #     return x * y   
+    
+    def calculated_total_price(self):
+        return self.price * self.quantity
+        
+item_1 = Item("iPhone", 100, -5)
+item_2 = Item("Laptop", 1000, 3)
+
+print(item_2.name)
+
+        
+# print(item_1.calculated_total_price(item_1.price, item_1.quantity))
+print(item_1.calculated_total_price())
+# print(item_2.calculated_total_price())
