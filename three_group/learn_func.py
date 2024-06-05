b9b372fe801e7122532a72fa95f9ef8ffec21b22
Nevojt <nevojt@gmail.com>
2024-06-05 19:34:46 +0200

    school

---
+++ three_group/learn_func.py
@@ -0,0 +1,31 @@
+# from math import pi
+# # def test():
+# #     return "test"
+    
+    
+# # print(test())
+
+# def one_argument(a):
+#     return a
+
+# one = one_argument(pi)
+# # print(one)
+
+# def two_argument(a, b):
+#     return [a + b,  b + a]
+
+# two = two_argument("Hello ", "Word")
+# # two.reverse()
+# print(two[::-1])
+
+def three_argument(age: int, age2: int, name: str):
+    """
+    age: int Age user
+    age2: int Age minimum
+    name: str Name user
+    """
+    pass
+
+
+three_argument(12, 45, "Youra")
+
