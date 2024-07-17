db58e22a068105e2dccd1ce2840bb2f93311f670
Dmytro Nevoit <119137577+Nevojt@users.noreply.github.com>
2024-07-17 22:45:38 +0200

    Update Banker Roulette.py

--- Banker Roulette.py
+++ Banker Roulette.py
@@ -3,6 +3,9 @@ import random
 names_string = input("Give me everybody's names, separated by a comma. ")
 names = names_string.split(", ")
 
+#
 name_list = random.randint(0, len(names))
 name_cash = names[name_list] 
 print(f"{name_cash} is going to buy the meal today!")
+
+
